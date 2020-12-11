84c6b8d8f26ee609fb7d0f6d43cac16a459f43af
Samuel Sincos <samuel.edi@student.unri.ac.id>
2020-12-11 12:25:37 +0700

    Ajax Response
    Change Ajax Response

--- public/lav/js/lx.js
+++ public/lav/js/lx.js
@@ -1,22 +1,44 @@
-$("#tipe").change(function() {
-  var tipew = $(this).val();    
-  if(tipew) {
-    $.ajax({
-      type:"GET",
-      url:"/ajx?tipe="+tipew,
-      success:function(data){               
-        if (data.length != 0) {
-          $("#jenis").empty();
-          $("#jenis").append('<option>Pilih Jenis</option>');
-          $.each(data, function(key, value){
-            $("#jenis").append('<option value="'+value+'">'+value+'</option>');
-          });       
-        } else { 
-          $("#jenis").empty();
-        }
-      }
-    });
-  } else {
-    $("#jenis").empty();
-  }      
+$("#tipe").change(() => {
+  let types = $(this).val();
+
+  if(types) {
+    let objAjaxTypes = {
+      method: "GET",
+      url: "/ajx?tipe=" + types,
+      data: ""
+    }
+
+    ajaxResponse(typesCallback, objAjaxTypes);
+  } 
+  else
+    $("#jenis").empty();   
 });
+
+function typesCallback(data) {
+  if (data.length != 0) {
+    $("#jenis").empty();
+    $("#jenis").append('<option>Pilih Jenis</option>');
+
+    for (let types in data)
+      $("#jenis").append('<option value="' + types + '">' + types + '</option>');
+  } 
+  else
+    $("#jenis").empty();
+}
+
+function ajaxResponse(callback, properties, ...arguments) {
+  $.ajax({
+    type: properties.method,
+    url: properties.url,
+    data: JSON.stringify(properties.data),
+    contentType: "application/json;",
+    dataType: "json",
+    cache: false,
+    success: (response) => {
+      return callback(response, ...arguments);
+    },
+    error: (e) => {
+      return console.log("Refresh Halaman \n Error Text: " + e.responseText);
+    }
+  });
+}
