a636c8080efdf0d7eb948f545851ada73f7948bc
Samuel Sincos <samuel.edi@student.unri.ac.id>
2020-12-16 23:33:01 +0700

    Update
    Update

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// url http:127.0.0.1:8000/api/
+# URL http:127.0.0.1:8000/api/
 Route::post('/ajaxtypes','TransaksiController@show');
 Route::post('/search','TransaksiController@search');
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@
 
 Route::get('/', 'HomeController@index');
 
-//CRUD Kategori
+# CRUD Kategori
 Route::get('/kategori', 'KategoriController@index');
 Route::get('/kategori/add', 'KategoriController@create');
 Route::post('/kategori/store', 'KategoriController@store');
@@ -25,7 +25,7 @@ Route::get('/kategori/edit/{id}', 'KategoriController@edit');
 Route::post('/kategori/update/{id}', 'KategoriController@update');
 Route::get('/kategori/hapus/{id}', 'KategoriController@destroy');
 
-//CRUD Transaksi
+# CRUD Transaksi
 Route::get('/transaksi', 'TransaksiController@index');
 Route::get('/transaksi/add', 'TransaksiController@create');
 Route::post('/transaksi/store', 'TransaksiController@store');
@@ -33,6 +33,6 @@ Route::get('/transaksi/edit/{id}', 'TransaksiController@edit');
 Route::post('/transaksi/update/{id}', 'TransaksiController@update');
 Route::get('/transaksi/hapus/{id}', 'TransaksiController@destroy');
 
-//ajax
+# Ajax
 Route::post('/ajaxtypes','TransaksiController@show');
 Route::post('/search','TransaksiController@search');
